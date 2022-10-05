0feee14917f27761e835d37c4faedbbd17be464c
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-10-05 17:17:10 +0300

    decoding jason  data from api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:cilma_mussa/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = 'ca22c53dc6c5f364e8e792c3f4281327';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -13,30 +16,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class LoadingScreenState extends State<LoadingScreen> {
+  double? longitude;
+  double? latitude;
+
   @override
   void initState() {
     super.initState();
-    // getLocation();
-    getData();
+    getLocation();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print('longitude :${location.longitude}');
-    print('latitude :${location.latitude}');
+    longitude = location.longitude;
+    latitude = location.latitude;
+    getData();
   }
 
   late final String url =
-      'http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid=ca22c53dc6c5f364e8e792c3f4281327';
-//
+      'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+
   void getData() async {
     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+      var decodrdData = jsonDecode(data);
+
+      var cond = decodrdData['weather'][0]['id'];
+      var temp = decodrdData['main']['temp'];
+      var cName = decodrdData['name'];
+      print(cName);
+      print(cond);
+      print(temp);
     } else {
-      print('state ${response.statusCode}');
+      print(response.statusCode);
     }
   }
 
